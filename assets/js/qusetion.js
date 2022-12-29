73326c86a69989960a264e7cadd2fb251ec6e3de
Dee68 <ddimie283@gmail.com>
2022-12-29 23:07:24 +0000

    Add quiz level functionality.

--- assets/js/qusetion.js
+++ assets/js/qusetion.js
@@ -1,4 +1,5 @@
-let  DzQuiz = [
+let DzQuiz  = [
+    [
     {   num: 1,
         question: "What does HTML stand for ?",
         choices:[ 
@@ -21,14 +22,14 @@ let  DzQuiz = [
 
     },
     {   num: 3,
-        question: "Which HTML tag is used to define an internal style sheet ?",
+        question: "What does XML stand for ?",
         choices:[
-        " '<'script'>' ",
-        " <style> ",
-        " <html> ",
-        " <svg> ",
+        "Extensible Markup Language",
+        "Executable Multiple Language",
+        "Extra Multi Program Language",
+        "Examine Multi Language",
         ],
-        answer: "<style>"
+        answer: "Extensible Markup Language"
     },
     {   num: 4,
         question: "Which is the correct CSS synthax ?",
@@ -100,5 +101,111 @@ let  DzQuiz = [
         ],
         answer: "<!-- This is a comment -->"
     }
+    ],
+    
+    [
+        {   num: 1,
+            question: "What does HTML stand for ?",
+            choices:[ 
+            "Hyperlinks and Text Markup Language",
+            "Hyper Text Markup Language",
+            "hyper Text Making Languaage",
+            "Hyper Text Make Language",
+            ],
+            answer: "Hyperlinks and Text Markup Language"
+        },
+        {   num: 2,
+            question: "What does CSS stand for?",
+            choices:[
+            "Colourful Style Sheet",
+            "Creative Style Sheet",
+            "Cascading Style Sheet",
+            "Computer Style Sheeet",
+            ],
+            answer: "Cascading Style Sheet"
+    
+        },
+        {   num: 3,
+            question: "What does XML stand for ?",
+            choices:[
+            "Extensible Markup Language",
+            "Executable Multiple Language",
+            "Extra Multi Program Language",
+            "Examine Multi Language",
+            ],
+            answer: "Extensible Markup Language"
+        },
+        {   num: 4,
+            question: "Which is the correct CSS synthax ?",
+            choices:[
+            "body{color:black;}",
+            "{body{color:black;}",
+            "body=color:black;",
+            "body:{color=black}",
+            ],
+            answer: "body{color:black;}"
+        },
+        {   num: 5,
+            question: "How do you insert a comment in a CSS file ?",
+            choices:[
+            "/* this is a comment */",
+            " //This is a comment ",
+            " <!-- This is a comment --> ",
+            " //This is a comment // ",
+            ],
+            answer: "/* this is a comment */"
+        },
+        {   num: 6,
+            question: "How do you insert a comment in an HTML file ?",
+            choices:[
+            " /* this is a comment */ ",
+            " //This is a comment ",
+            " <!-- This is a comment --> ",
+            " //This is a comment // ",
+            ],
+            answer: "<!-- This is a comment -->"
+        },
+        {   num: 7,
+            question: "Which property is used to change the background color in CSS ?",
+            choices:[
+            "background-color",
+            "backgroundColor",
+            "bgColor",
+            "color-background",
+            ],
+            answer: "background-color" 
+        },
+        {   num: 8,
+            question: "Inside which HTML tag do we put the javascript ?",
+            choices:[
+            " '<javascript></javascript>' ",
+            "'<js></js>'",
+            "'<script></script>'",
+            "'<scripting>'",
+            ],
+            answer: "<script></script>"
+        },
+        {   num: 9,
+            question: "Which attributes are neccessary for the <img> tag in HTML ?",
+            choices:[
+            "alt and data",
+            "data and source",
+            "src and alt",
+            "alt and source",
+            ],
+            answer: "src and alt"
+        },
+        {   num: 10,
+            question: "How do you insert a comment in an HTML file ?",
+            choices:[
+            "/* this is a comment */",
+            "//This is a comment",
+            "<!-- This is a comment -->",
+            "//This is a comment //",
+            ],
+            answer: "<!-- This is a comment -->"
+        }
+
+    ]
 
 ];
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,23 @@
-// start button
 let start = document.getElementById('start');
-
 // rules div
 let rules = document.getElementById('rules');
+// exit button
+let quitBtn = document.getElementById('quit');
+// start button clicked
+function openPopup(){
+    start.classList.add('inactive');//hides start button
+    rules.classList.add('active'); // shows rules of quiz div
+}
+start.addEventListener('click', openPopup, false);
 
-// quiz div
-let quiz = document.getElementById('quiz');
-// timer
-let timeDiv = document.getElementById('time');
-
-let timeSec = document.getElementById('timer-sec');
-
-// question section
-let questions = document.getElementById('quiz-questions');
-let questionNum = document.getElementById('question-num');
-let questionText = document.getElementById('question-text');
-
-// answer section
-let spans = document.getElementsByClassName('answers');
-
-let answer1 = document.getElementById('answer1');
-let answer2 = document.getElementById('answer2');
-let answer3 = document.getElementById('answer3');
-let answer4 = document.getElementById('answer4');
-
-// next button
-let totalQ = document.getElementById('total');
-let nextQ = document.getElementById('nextQ');
+// hides rules div, quitBtn clicked
+function closePopup(){
+    rules.classList.remove('active');
+    start.classList.remove('inactive');//shows start button
+}
+quitBtn.addEventListener('click', closePopup, false);
+//continue button
+let continueBtn = document.getElementById('continue');
 
 // user registration div
 let userReg = document.getElementById('register');
@@ -35,90 +26,119 @@ let rForm = document.getElementById('register-form');
 
 let username = document.getElementById('username');
 
-// exit button
-let exit = document.getElementById('quit');
-
-//continue button
-let continueBtn = document.getElementById('continue');
-
-// registraion button
-let register = document.getElementById('register');
-
+// popup registration div,clicking continueBtn
+function userRegistration(){
+    userReg.classList.add('show');//shows div
+    rules.classList.remove('active');//hides
+    start.classList.add('inactive');//hides
+}
+continueBtn.addEventListener('click', userRegistration, false);
 
 //thank you div/button
 let welcome = document.getElementById('welcome');
+// thank you button 
+let thankYouBtn = document.getElementById('thank-you');
 
-let thankYou = document.getElementById('thank-you');
+// submit form to collect username & save to local storage
+rForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    //console.log(username.value);
+    localStorage.setItem('username', username.value);
+    const  user = localStorage.getItem('username');
+    document.getElementById('user').textContent = user;
+    welcome.classList.add('active');//show
+    userReg.classList.remove('show');//hide
+    
+});
+
+// quiz div
+let quiz = document.getElementById('quiz');
+// Start Quiz after registration and welcome message.
+function goQuiz(){
+    welcome.classList.remove('active');//hide
+    start.classList.remove('active');//hide
+    quiz.classList.add('active');//show
+    // count down begins once question is loaded.
+    countTime(timer);
+    //show question with answers
+    loadQuestion(index);
+    questCounter(questC);
+ }
+// thankYouBtn.addEventListener('click', goQuiz, false);
+// timer
+let timeSec = document.getElementById('timer-sec');
+// question section
+let questions = document.getElementById('quiz-questions');
+// next button
+let totalQ = document.getElementById('total');
+let nextQ = document.getElementById('nextQ');
 
 //answer option buttons
 let options = document.getElementsByClassName('option');
 
-let option_list = document.getElementsByClassName('option-list');
-
-
-
-
-// define variables for timer & correct answers
+// feed back
+let feedBack = document.getElementById('feed-back');
+ 
+// define variables for timer & correct answers & quiz level
+let qLevelTime = [15,10,10];
+let level = [0,1];// 1st & 2nd level of quiz
+let qlevel;
 let index = 0;
-let timer = 0;
+let timer = qLevelTime[0];
 let interval = 0;
+let counter;
 let correct = 0;
 let questC = 1;
-let userAns = undefined;
+
+qlevel = level[0];
 
 //footer question of questions
 function questCounter(questC){
-    let qTag = `<span><p>${questC++}</p> of <p>${DzQuiz.length}</p>Questions</span>`
+    
+    let qTag = `<span><p>${questC++}</p> of <p>${DzQuiz[qlevel].length}</p>Questions</span>`;
     totalQ.innerHTML = qTag;
 }
 
-function countDown(){
-    if(timer === 20){
-        clearInterval(interval);
-    }else{
-        timer++;
-        timeSec.innerText = timer;
+// timer function
+function countTime(timer){
+    counter = setInterval(timmer, 1000);
+    function timmer(){
+        timeSec.textContent = timer;
+        timer--;
+        if (timer < 0) {
+            clearInterval(counter);
+            timeSec.textContent = "00";
+        }
     }
 }
 
-//setInterval(countDown, 1000);
 
-// let loadData = function(){
-//     questionNum.innerText = index + 1+".";
-//     questionText.innerText = DzQuiz[index].question;
-//     answer1.innerText = DzQuiz[index].option1;
-//     answer2.innerText = DzQuiz[index].option2;
-//     answer3.innerText = DzQuiz[index].option3;
-//     answer4.innerText = DzQuiz[index].option4;
-//     // start timer
-//     timer = 0;
-// };
-// loadData();
 // load Questions
 function loadQuestion(index){
     let quesTag = `<div class="questions">
-    <span id="question-num"> ${DzQuiz[index].num}</span>
-    <span id="question-text">${DzQuiz[index].question}</span></div>
+    <span id="question-num">${DzQuiz[qlevel][index].num +'.'}</span>
+    <span id="question-text">${DzQuiz[0][index].question}</span></div>
     <div class="option-list">
                     <div class="option">
-                        <span id="answer1" class="answers">${DzQuiz[index].choices[0]}</span>
-                        <div class="icon tick"><i class="fas fa-check"></i></div>
+                        <span id="answer1" class="answers">${DzQuiz[qlevel][index].choices[0]}</span>
+                        <div class="icon"></div>
                     </div>
                     <div class="option">
-                        <span id="answer2" class="answers">${DzQuiz[index].choices[1]}</span>
-                        <div class="icon cross"><i class="fas fa-times"></i></div>
+                        <span id="answer2" class="answers">${DzQuiz[0][index].choices[1]}</span>
+                        <div class="icon"></div>
                     </div>
                     <div class="option">
-                        <span id="answer3" class="answers">${DzQuiz[index].choices[2]}</span>
-                        <div class="icon cross"><i class="fas fa-times"></i></div>
+                        <span id="answer3" class="answers">${DzQuiz[qlevel][index].choices[2]}</span>
+                        <div class="icon"></div>
                     </div>
                     <div class="option">
-                        <span id="answer4" class="answers">${DzQuiz[index].choices[3]}</span>
-                        <div class="icon cross"><i class="fas fa-times"></i></div>
+                        <span id="answer4" class="answers">${DzQuiz[qlevel][index].choices[3]}</span>
+                        <div class="icon"></div>
                     </div>
                 </div>`;
 questions.innerHTML = quesTag;
 // user clicking an option
+
 for (let i = 0; i < options.length; i++) {
     options[i].setAttribute('onclick', 'optionSelected(this)');
     
@@ -128,71 +148,119 @@ for (let i = 0; i < options.length; i++) {
 
 //user clicked an answer
 function optionSelected(answ){
+    clearInterval(counter);
     let userAnswer = answ.textContent;
-
-    let correctAns = DzQuiz[index].answer;
-    
+    let corTag = `<i class="fas fa-check"></i>`;
+    let incorTag = `<i class="fas fa-times"></i>`;
+    let correctAns = DzQuiz[qlevel][index].answer;
+   
     if (userAnswer.trim() == correctAns.trim()) {
         console.log('correct');
+        correct++;
+        answ.lastElementChild.innerHTML = corTag;
         answ.classList.remove('option');
         answ.classList.add('correct');
     } else {
         console.log('incorrect')
+        answ.lastElementChild.innerHTML = incorTag;
         answ.classList.add('incorrect');
+        //if incorrect answer automatically show correct answer
+        for (let i = 0; i < options.length; i++) {
+            if (options[i].textContent.trim() == correctAns.trim()) {
+                options[i].setAttribute('class','option correct');
+                options[i].lastElementChild.innerHTML = corTag;
+            }
+            
+        }
     }
-    // disable all other options after user's choice
+    //
+    localStorage.setItem('correctAnswer', correct);
+
+     //stop timer
+     clearInterval(interval);
+    // disable all  options after user's choice
+    for (let i = 0; i < options.length; i++) {
+        options[i].classList.add('disabled');  
+    }
    
+    nextQ.style.display = 'block';
 
 }
 
-function openPopup(){
-    start.classList.add('inactive');
-    rules.classList.add('active');
-}
-//
-function closePopup(){
-    rules.classList.remove('active');
-    start.classList.remove('inactive');
-}
-// popup registration div
-function userRegistration(){
-    userReg.classList.add('show');
-    rules.classList.remove('active');
-    start.classList.add('inactive');
-}
 
-// submit form to collect username
-rForm.addEventListener('submit', function(e){
-    e.preventDefault();
-    //console.log(username.value);
-    localStorage.setItem('username', username.value);
-    const  user = localStorage.getItem('username');
-    document.getElementById('user').textContent = user;
-    welcome.classList.add('active');
-    userReg.classList.remove('show');
-    
-});
 // next button clicked
 function nextQuestion(){
-    if (index < DzQuiz.length) {
+    localStorage.setItem('username', username.value);
+    const  user = localStorage.getItem('username');
+    const correctAnswer = localStorage.getItem('correctAnswer');
+    let feedtag;
+    if (correctAnswer >= 5) {
+        feedtag = `
+        <i class="fas fa-trophy"></i>
+        <h4>Hi ${user} you have completed the quiz</h4>
+        <h4 id="points">You got ${correctAnswer} out of 10</h4>
+        <button id="exit" onclick="returnHome()">Quit Quiz</button>
+        <button id="move-up" onclick="quizLevel2()">Next Level</button>`;
+        
+    } else {
+        feedtag = `<i class="fas fa-meh-rolling-eyes"></i>
+            <h4>Hi ${user} you have completed the quiz</h4>
+            <h4 id="points">You got ${correctAnswer} out of 10</h4>
+            <button id="exit" onclick="returnHome()">Quit Quiz</button>
+            <button id="start-again">Start Again</button>`; 
+    }
+    
+
+    if (index < DzQuiz[qlevel].length - 1) {
         index++;
         questC++;
         loadQuestion(index);
         questCounter(questC);
+        clearInterval(counter);
+        countTime(timer);
+        nextQ.style.display = 'none';
     } else {
-        console.log('questions completed');
+        quiz.classList.remove('active');
+        start.classList.remove('active');
+        feedBack.innerHTML = feedtag;
+        feedBack.classList.add('reveal');
+        // console.log('questions completed');
     }
 }
-// Start Quiz after registration and welcome message.
-function goQuiz(){
-   welcome.classList.remove('active');
-   start.classList.remove('active');
-   quiz.classList.add('active');
-   // count down begins once question is loaded.
-   interval = setInterval(countDown, 1000);
-   loadQuestion(index);
-   questCounter(questC);
+
+let moveUp = document.getElementById('move-up');
+
+function quizLevel2(){
+    feedBack.classList.remove('reveal');
+    // start.classList.remove('active');
+    // start.classList.add('inactive');
+    quiz.classList.add('active');//show
+    qlevel = level[1];
+    timer = qLevelTime[1];
+    questC = 1;
+    index = 0;
+    //questCounter(questC);
+    //loadQuestion(index);
+    //goQuiz();
+    countTime(timer);
+    //show question with answers
+    loadQuestion(index);
+    questCounter(questC);
+    optionSelected(answ);
+    //quiz.classList.add('active');
+    
 }
+
+let exitBtn = document.getElementById('exit');
+
+// exit quiz and return back to start page
+function returnHome(){
+    feedBack.classList.remove('reveal');
+    start.classList.remove('inactive');
+    start.classList.add('active');
+}
+
+exitBtn.addEventListener('click', returnHome, false);
 
 
 
