0d277f0ee7bb0ac7adfd6b2a47d09d0f16f386e0
Dee68 <ddimie283@gmail.com>
2023-01-07 20:47:52 +0000

    Add correction to code structure after jshint warnings.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,225 +1,7 @@
-/*jshint esversion: 8 */
-/* exported quizLevel2,returnHome,optionSelected,startAgain */
-const DzQuiz  = [
-    [
-    {   num: 1,
-        question: "What does HTML stand for ?",
-        choices:[ 
-        "Hyperlinks and Text Markup Language",
-        "Hyper Text Markup Language",
-        "hyper Text Making Languaage",
-        "Hyper Text Make Language",
-        ],
-        answer: "Hyperlinks and Text Markup Language"
-    },
-    {   num: 2,
-        question: "What does CSS stand for?",
-        choices:[
-        "Colourful Style Sheet",
-        "Creative Style Sheet",
-        "Cascading Style Sheet",
-        "Computer Style Sheeet",
-        ],
-        answer: "Cascading Style Sheet"
+/*jshint esversion: 6 */
 
-    },
-    {   num: 3,
-        question: "What does XML stand for ?",
-        choices:[
-        "Extensible Markup Language",
-        "Executable Multiple Language",
-        "Extra Multi Program Language",
-        "Examine Multi Language",
-        ],
-        answer: "Extensible Markup Language"
-    },
-    {   num: 4,
-        question: "Which is the correct CSS synthax ?",
-        choices:[
-        "body{color:black;}",
-        "{body{color:black;}",
-        "body=color:black;",
-        "body:{color=black}",
-        ],
-        answer: "body{color:black;}"
-    },
-    {   num: 5,
-        question: "How do you add a background color for all h1 elements ?",
-        choices:[
-        "h1.all {background-color:#FFFFFF;}",
-        "all.h1 {background-color:#FFFFFF;}",
-        "h1 {background-color:#FFFFFF;}",
-        "all.h1 {color-background:#FFFFFF;}",
-        ],
-        answer: "h1 {background-color:#FFFFFF;}"
-    },
-    {   num: 6,
-        question: "Which CSS property is used to change the text color of an element ?",
-        choices:[
-        "text-color",
-        "color",
-        "fgcolor",
-        "color-text",
-        ],
-        answer: "color"
-    },
-    {   num: 7,
-        question: "Which property is used to change the background color in CSS ?",
-        choices:[
-        "background-color",
-        "backgroundColor",
-        "bgColor",
-        "color-background",
-        ],
-        answer: "background-color" 
-    },
-    {   num: 8,
-        question: "Which CSS property controls the text size ?",
-        choices:[
-        "font-size",
-        "text-size",
-        "size",
-        "font-style",
-        ],
-        answer: "font-size"
-    },
-    {   num: 9,
-        question: "Which attributes are neccessary for the &lt;img&gt; tag in HTML ?",
-        choices:[
-        "alt and data",
-        "data and source",
-        "src and alt",
-        "alt and source",
-        ],
-        answer: "src and alt"
-    },
-    {   num: 10,
-        question: "How do you display hyperlinks without an underline ?",
-        choices:[
-        "a {underline:none;}",
-        "a {text-decoration:none;} ",
-        "a {decoration:no-underline;}",
-        "a {text-decoration:no-underline;}",
-        ],
-        answer: "a {text-decoration:none;} "
-    }
-    ],
-    
-    [
-        {   num: 1,
-            question: "The HTML &lt;canvas&gt; element is used to: ?",
-            choices:[ 
-            "draw graphics",
-            "manipulate data in MySQL",
-            "display database records",
-            "create draggable elements",
-            ],
-            answer: "draw graphics"
-        },
-        {   num: 2,
-            question: "In HTML, which attribute is used to specify that an input field must be filled out",
-            choices:[
-            "placeholder",
-            "validate",
-            "required",
-            "formvalidate",
-            ],
-            answer: "required"
-    
-        },
-        {   num: 3,
-            question: "Which input type defines a slider control ?",
-            choices:[
-            "slider",
-            "controls",
-            "gauge",
-            "range",
-            ],
-            answer: "range"
-        },
-        {   num: 4,
-            question: "Which HTML attribute is used to define inline styles ?",
-            choices:[
-            "style",
-            "class",
-            "styles",
-            "font",
-            ],
-            answer: "style"
-        },
-        {   num: 5,
-            question: "Who is making the web standards ?",
-            choices:[
-            "Microsoft",
-            "Google",
-            "The World Wide Web Consortium",
-            "Mozilla",
-            ],
-            answer: "The World Wide Web Consortium"
-        },
-        {   num: 6,
-            question: "Which HTML attribute specifies an alternate text for an image, if the image cannot be displayed ?",
-            choices:[
-            "longdesc",
-            "alt",
-            "src",
-            "title",
-            ],
-            answer: "alt"
-        },
-        {   num: 7,
-            question: "Which property is used to change the background color in CSS ?",
-            choices:[
-            "background-color",
-            "backgroundColor",
-            "bgColor",
-            "color-background",
-            ],
-            answer: "background-color" 
-        },
-        {   num: 8,
-            question: "In HTML, onblur and onfocus are: ?",
-            choices:[
-            "Style attributes",
-            "HTML elements",
-            "Can only be used in Javascript",
-            "Event attributes",
-            ],
-            answer: "Event attributes"
-        },
-        {   num: 9,
-            question: "Which attributes are neccessary for the &lt;img&gt; tag in HTML ?",
-            choices:[
-            "alt and data",
-            "data and source",
-            "src and alt",
-            "alt and source",
-            ],
-            answer: "src and alt"
-        },
-        {   num: 10,
-            question: "Graphics defined by SVG is in which format ?",
-            choices:[
-            "HTML",
-            "XML",
-            "CSS",
-            "Javascript",
-            ],
-            answer: "XML"
-        }
-
-    ]
-
-];
-const start = document.getElementById('start');
-// rules div
 const rules = document.getElementById('rules');
-// exit button
-const quitBtn = document.getElementById('quit');
-
-//continue button
 const continueBtn = document.getElementById('continue');
-//thank you div
 const welcome = document.getElementById('welcome');
 //thank you button
 const thankYou = document.getElementById('thank-you');
@@ -229,23 +11,32 @@ const msg = document.getElementById('message');
 const quiz = document.getElementById('quiz');
 // user registration div
 const userReg = document.getElementById('register');
-
 const rForm = document.getElementById('register-form');
-
 const username = document.getElementById('username');
 // timer
 const timeSec = document.getElementById('timer-sec');
 // question section
-const questions = document.getElementById('quiz-questions');
+const questNum = document.getElementById('question-num');
+const questText = document.getElementById('question-text');
 // next button
 const totalQ = document.getElementById('total');
 
 const nextQ = document.getElementById('nextQ');
 //answer option buttons
-const options = document.getElementsByClassName('option');
+const options = Array.from(document.getElementsByClassName('option'));
+const answer1 = document.getElementById('answer1');
+const answer2 = document.getElementById('answer2');
+const answer3 = document.getElementById('answer3');
+const answer4 = document.getElementById('answer4');
 
 // feed back
 const feedBack = document.getElementById('feed-back');
+const face = document.getElementById('face');
+const info = document.getElementById('info');
+const points = document.getElementById('points');
+const nextlevel = document.getElementById('move-up');
+const playAgain = document.getElementById('start-again');
+const exBtn = document.getElementById('exit');
 // define variables for timer & correct answers & quiz level
 let qLevelTime = [10,5];
 let level = [0,1];// 1st & 2nd level of quiz
@@ -257,7 +48,7 @@ let counter;
 let correct = 0;
 let questC = 1;
 localStorage.setItem('username', 'username');
-localStorage.setItem('correctAnswer', 'correct');
+localStorage.setItem('correctAnswer', 0);
 
 qlevel = level[0];
 
@@ -276,26 +67,17 @@ function quizLevel2(){
     questCounter(questC);  
 }
 
-// start button clicked
-function openPopup(){
-    start.classList.add('inactive');//hides start button
-    rules.classList.add('active'); // shows rules of quiz div
-}
-start.addEventListener('click', openPopup, false);
+nextlevel.addEventListener('click', function(e){
+    if (e == 'click') {
+        quizLevel2();
+    }
+});
+
    
-// hides rules div, quitBtn clicked
-function closePopup(){
-    rules.classList.remove('active');
-    start.classList.remove('inactive');//shows start button
-}
-quitBtn.addEventListener('click', closePopup, false);
-
-
 // popup registration div,clicking continueBtn
 function userRegistration(){
     userReg.classList.add('show');//shows div
-    rules.classList.remove('active');//hides
-    start.classList.add('inactive');//hides
+    rules.classList.add('inactive');//hides
 }
 continueBtn.addEventListener('click', userRegistration, false);
 
@@ -313,7 +95,6 @@ function validateName(){
         return true;
     }
 }
-
 
 // submit form to collect username & save to local storage
 rForm.addEventListener('submit', function(e){
@@ -340,12 +121,12 @@ thankYou.addEventListener('click', function(e){
 // Start Quiz after registration and welcome message.
 function goQuiz(){
     welcome.classList.remove('active');//hide
-    start.classList.remove('active');//hide
     quiz.classList.add('active');//show
     // count down begins once question is loaded.
     countTime(timer);
     //show question with answers
     loadQuestion(index);
+  
     questCounter(questC);
  }
 
@@ -361,14 +142,13 @@ let exitBtn = document.getElementById('leave');
 exitBtn.addEventListener('click', function(){
     quiz.classList.remove('active');
     localStorage.clear();
-    start.classList.remove('inactive');
-    start.classList.add('active');
+    username = '';
+    correctAnswer = 0;
 });
 
 //footer question of questions
 function questCounter(questC){
-    
-    let qTag = `<span><p>${questC++}</p> of <p>${DzQuiz[qlevel].length}</p>Questions</span>`;
+    let qTag = `<span><p>${questC++}</p> <span><p>of </p></span><p>${DzQuiz[qlevel].length}</p><span><p>Questions</p></span>`;
     totalQ.innerHTML = qTag;
 }
 
@@ -392,42 +172,35 @@ function countTime(timer){
 
 
 // load Questions
-function loadQuestion(index){
-    let quesTag = `<div class="questions">
-    <span id="question-num">${DzQuiz[qlevel][index].num +'.'}</span>
-    <span id="question-text">${DzQuiz[qlevel][index].question}</span></div>
-    <div class="option-list">
-                    <div class="option">
-                        <span id="answer1" class="answers">${DzQuiz[qlevel][index].choices[0]}</span>
-                        <div class="icon"></div>
-                    </div>
-                    <div class="option">
-                        <span id="answer2" class="answers">${DzQuiz[qlevel][index].choices[1]}</span>
-                        <div class="icon"></div>
-                    </div>
-                    <div class="option">
-                        <span id="answer3" class="answers">${DzQuiz[qlevel][index].choices[2]}</span>
-                        <div class="icon"></div>
-                    </div>
-                    <div class="option">
-                        <span id="answer4" class="answers">${DzQuiz[qlevel][index].choices[3]}</span>
-                        <div class="icon"></div>
-                    </div>
-                </div>`;
-questions.innerHTML = quesTag;
-// user clicking an option
+function loadQuestion(index){ 
+    questNum.innerHTML = `${DzQuiz[qlevel][index].num +'.'}`;
+    questText.innerHTML = `${DzQuiz[qlevel][index].question}`;
+    answer1.innerHTML = `${DzQuiz[qlevel][index].choices[0]}`;
+    answer2.innerHTML = `${DzQuiz[qlevel][index].choices[1]}`;
+    answer3.innerHTML = `${DzQuiz[qlevel][index].choices[2]}`;
+    answer4.innerHTML = `${DzQuiz[qlevel][index].choices[3]}`;
 
-for (let i = 0; i < options.length; i++) {
-    options[i].setAttribute('onclick', 'optionSelected(this)');
-    
-}
+   
+// before user clicks an option
+
+options.forEach(function(removeActive){
+    removeActive.classList.remove('correct');
+    removeActive.classList.remove('incorrect');
+    removeActive.classList.remove('disabled');
+    removeActive.classList.add('option');
+    removeActive.lastElementChild.innerHTML = '';
+});
+
 
 }
 
 //user clicked an answer
-function optionSelected(answ){
+
+options.forEach(function(option){
+    option.addEventListener('click', function(){
+       
     clearInterval(counter);
-    let userAnswer = answ.textContent;
+    let userAnswer = option.textContent;
     let corTag = `<i class="fas fa-check"></i>`;
     let incorTag = `<i class="fas fa-times"></i>`;
     let correctAns = DzQuiz[qlevel][index].answer;
@@ -436,13 +209,13 @@ function optionSelected(answ){
     if (userAnswer.trim() == correctAns.trim()) {
         //console.log('correct');
         correct++;
-        answ.lastElementChild.innerHTML = corTag;
-        answ.classList.remove('option');
-        answ.classList.add('correct');
+        option.lastElementChild.innerHTML = corTag;
+        option.classList.remove('option');
+        option.classList.add('correct');
     } else {
         //console.log('incorrect')
-        answ.lastElementChild.innerHTML = incorTag;
-        answ.classList.add('incorrect');
+        option.lastElementChild.innerHTML = incorTag;
+        option.classList.add('incorrect');
         //if incorrect answer automatically show correct answer
         for (let i = 0; i < options.length; i++) {
             if (options[i].textContent.trim() == correctAns.trim()) {
@@ -455,7 +228,6 @@ function optionSelected(answ){
     //store correct answer
      localStorage.setItem('correctAnswer', correct);
     
-
      //stop timer
      clearInterval(interval);
     // disable all  options after user's choice
@@ -465,48 +237,42 @@ function optionSelected(answ){
    
     nextQ.style.display = 'block';
 
-}
 
+    });
+});
 
 // next button clicked
 function nextQuestion(){
-    
     const  user = localStorage.getItem('username');
-    
     const correctAnswer = localStorage.getItem('correctAnswer');
     
-    
-    let feedtag;
     if (qlevel == 0 && correctAnswer >= 5) {
-        feedtag = `
-        <i class="fas fa-trophy"></i>
-        <h4>Hi ${user}, congratulations you are good to go to the next level</h4>
-        <h4 id="points">You got ${correctAnswer} out of ${(qlevel+1)*10}</h4>
-        <button id="exit" class="next" onclick="returnHome()">Quit Quiz</button>`;
-        if (qlevel == 0) {
-            // exBtn.style.display = 'block';
-            feedtag += `<button id="move-up" onclick="quizLevel2()">Next Level</button>`;
-        }
-        
+        face.innerHTML = `<i class="fas fa-trophy"></i>`;
+        info.innerHTML = `Hi ${user}, congratulations you are good to go to the next level`;
+        points.innerHTML = `You got ${correctAnswer} out of ${(qlevel+1)*10}`;
+        if (qlevel == 0 ) {
+            nextlevel.style.display = 'block';
+            exBtn.style.display = 'block';
+            playAgain.style.display = 'none';
+        } 
         
     } else if (qlevel == 1 && correctAnswer >= 10){
-        feedtag = `
-        <i class="fas fa-trophy"></i>
-        <h4>Hi ${user}, congratulations you are good.</h4>
-        <h4 id="points">You got ${correctAnswer} out of ${(qlevel+1)*10}</h4>
-        <button id="exit" onclick="returnHome()">Quit Quiz</button>`;
-        
+        face.innerHTML = `<i class="fas fa-trophy"></i>`;
+        info.innerHTML = `Hi ${user}, congratulations you are good.`;
+        points.innerHTML = `You got ${correctAnswer} out of ${(qlevel+1)*10}`;
+        exBtn.style.display = 'block';
+        playAgain.style.display = 'none';
+        nextlevel.style.display = 'none';
     } 
     else {
-        feedtag = `<i class="fas fa-meh-rolling-eyes"></i>
-            <h4>Hi ${user}, sorry you need to brush up your skills and try again</h4>
-            <h4 id="points">You got ${correctAnswer} out of ${(qlevel+1)*10}</h4>
-            <button id="exit" onclick="returnHome()">Quit Quiz</button>
-            <button id="start-again" onclick="startAgain()">Start Again</button>`; 
-               
+        face.innerHTML = `<i class="fas fa-meh-rolling-eyes"></i>`;
+        info.innerHTML = `Hi ${user}, sorry you need to brush up your skills and try again.`;
+        points.innerHTML = `You got ${correctAnswer} out of ${(qlevel+1)*10}`;
+        exBtn.style.display = 'block';
+        playAgain.style.display = 'block';
+        nextlevel.style.display = 'none';        
     }
     
-
     if (index < DzQuiz[qlevel].length - 1) {
         index++;
         questC++;
@@ -517,8 +283,6 @@ function nextQuestion(){
         nextQ.style.display = 'none';
     } else {
         quiz.classList.remove('active');
-        start.classList.remove('active');
-        feedBack.innerHTML = feedtag;
         feedBack.classList.add('reveal');
         
     }
@@ -539,15 +303,8 @@ function startAgain(){
     
 }
 
-// exit quiz and return back to start page
-function returnHome(){
-    feedBack.classList.remove('reveal');
-    start.classList.remove('inactive');
-    start.classList.add('active');
-}
-
-
-
-
-
-    
+playAgain.addEventListener('click', function(e){
+    if (e  == 'click') {
+        startAgain();
+    }
+});
