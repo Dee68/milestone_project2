22ce7853da383af0b9795545d59e45535ce1ec09
Dee68 <ddimie283@gmail.com>
2023-01-11 14:46:44 +0000

    Add formatting to javascript files.

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,209 +1,227 @@
 /*jshint esversion: 6 */
 
-const dzQuiz  = [
-    [
-    {   num: 1,
-        question: "What does HTML stand for ?",
-        choices:[ 
-        "Hyperlinks and Text Markup Language",
-        "Hyper Text Markup Language",
-        "hyper Text Making Languaage",
-        "Hyper Text Make Language",
-        ],
-        answer: "Hyperlinks and Text Markup Language"
-    },
-    {   num: 2,
-        question: "What does CSS stand for?",
-        choices:[
-        "Colourful Style Sheet",
-        "Creative Style Sheet",
-        "Cascading Style Sheet",
-        "Computer Style Sheeet",
-        ],
-        answer: "Cascading Style Sheet"
+const dzQuiz = [
+    [{
+            num: 1,
+            question: "What does HTML stand for ?",
+            choices: [
+                "Hyperlinks and Text Markup Language",
+                "Hyper Text Markup Language",
+                "hyper Text Making Languaage",
+                "Hyper Text Make Language",
+            ],
+            answer: "Hyperlinks and Text Markup Language"
+        },
+        {
+            num: 2,
+            question: "What does CSS stand for?",
+            choices: [
+                "Colourful Style Sheet",
+                "Creative Style Sheet",
+                "Cascading Style Sheet",
+                "Computer Style Sheeet",
+            ],
+            answer: "Cascading Style Sheet"
 
-    },
-    {   num: 3,
-        question: "What does XML stand for ?",
-        choices:[
-        "Extensible Markup Language",
-        "Executable Multiple Language",
-        "Extra Multi Program Language",
-        "Examine Multi Language",
-        ],
-        answer: "Extensible Markup Language"
-    },
-    {   num: 4,
-        question: "Which is the correct CSS synthax ?",
-        choices:[
-        "body{color:black;}",
-        "{body{color:black;}",
-        "body=color:black;",
-        "body:{color=black}",
-        ],
-        answer: "body{color:black;}"
-    },
-    {   num: 5,
-        question: "How do you add a background color for all h1 elements ?",
-        choices:[
-        "h1.all {background-color:#FFFFFF;}",
-        "all.h1 {background-color:#FFFFFF;}",
-        "h1 {background-color:#FFFFFF;}",
-        "all.h1 {color-background:#FFFFFF;}",
-        ],
-        answer: "h1 {background-color:#FFFFFF;}"
-    },
-    {   num: 6,
-        question: "Which CSS property is used to change the text color of an element ?",
-        choices:[
-        "text-color",
-        "color",
-        "fgcolor",
-        "color-text",
-        ],
-        answer: "color"
-    },
-    {   num: 7,
-        question: "Which property is used to change the background color in CSS ?",
-        choices:[
-        "background-color",
-        "backgroundColor",
-        "bgColor",
-        "color-background",
-        ],
-        answer: "background-color" 
-    },
-    {   num: 8,
-        question: "Which CSS property controls the text size ?",
-        choices:[
-        "font-size",
-        "text-size",
-        "size",
-        "font-style",
-        ],
-        answer: "font-size"
-    },
-    {   num: 9,
-        question: "Which attributes are neccessary for the &lt;img&gt; tag in HTML ?",
-        choices:[
-        "alt and data",
-        "data and source",
-        "src and alt",
-        "alt and source",
-        ],
-        answer: "src and alt"
-    },
-    {   num: 10,
-        question: "How do you display hyperlinks without an underline ?",
-        choices:[
-        "a {underline:none;}",
-        "a {text-decoration:none;} ",
-        "a {decoration:no-underline;}",
-        "a {text-decoration:no-underline;}",
-        ],
-        answer: "a {text-decoration:none;} "
-    }
-    ],
-    
-    [
-        {   num: 1,
-            question: "The HTML &lt;canvas&gt; element is used to: ?",
-            choices:[ 
-            "draw graphics",
-            "manipulate data in MySQL",
-            "display database records",
-            "create draggable elements",
-            ],
-            answer: "draw graphics"
         },
-        {   num: 2,
-            question: "In HTML, which attribute is used to specify that an input field must be filled out",
-            choices:[
-            "placeholder",
-            "validate",
-            "required",
-            "formvalidate",
+        {
+            num: 3,
+            question: "What does XML stand for ?",
+            choices: [
+                "Extensible Markup Language",
+                "Executable Multiple Language",
+                "Extra Multi Program Language",
+                "Examine Multi Language",
             ],
-            answer: "required"
-    
+            answer: "Extensible Markup Language"
         },
-        {   num: 3,
-            question: "Which input type defines a slider control ?",
-            choices:[
-            "slider",
-            "controls",
-            "gauge",
-            "range",
+        {
+            num: 4,
+            question: "Which is the correct CSS synthax ?",
+            choices: [
+                "body{color:black;}",
+                "{body{color:black;}",
+                "body=color:black;",
+                "body:{color=black}",
             ],
-            answer: "range"
+            answer: "body{color:black;}"
         },
-        {   num: 4,
-            question: "Which HTML attribute is used to define inline styles ?",
-            choices:[
-            "style",
-            "class",
-            "styles",
-            "font",
+        {
+            num: 5,
+            question: "How do you add a background color for all h1 elements ?",
+            choices: [
+                "h1.all {background-color:#FFFFFF;}",
+                "all.h1 {background-color:#FFFFFF;}",
+                "h1 {background-color:#FFFFFF;}",
+                "all.h1 {color-background:#FFFFFF;}",
             ],
-            answer: "style"
+            answer: "h1 {background-color:#FFFFFF;}"
         },
-        {   num: 5,
-            question: "Who is making the web standards ?",
-            choices:[
-            "Microsoft",
-            "Google",
-            "The World Wide Web Consortium",
-            "Mozilla",
+        {
+            num: 6,
+            question: "Which CSS property is used to change the text color of an element ?",
+            choices: [
+                "text-color",
+                "color",
+                "fgcolor",
+                "color-text",
             ],
-            answer: "The World Wide Web Consortium"
+            answer: "color"
         },
-        {   num: 6,
-            question: "Which HTML attribute specifies an alternate text for an image, if the image cannot be displayed ?",
-            choices:[
-            "longdesc",
-            "alt",
-            "src",
-            "title",
-            ],
-            answer: "alt"
-        },
-        {   num: 7,
+        {
+            num: 7,
             question: "Which property is used to change the background color in CSS ?",
-            choices:[
-            "background-color",
-            "backgroundColor",
-            "bgColor",
-            "color-background",
+            choices: [
+                "background-color",
+                "backgroundColor",
+                "bgColor",
+                "color-background",
             ],
-            answer: "background-color" 
+            answer: "background-color"
         },
-        {   num: 8,
-            question: "In HTML, onblur and onfocus are: ?",
-            choices:[
-            "Style attributes",
-            "HTML elements",
-            "Can only be used in Javascript",
-            "Event attributes",
+        {
+            num: 8,
+            question: "Which CSS property controls the text size ?",
+            choices: [
+                "font-size",
+                "text-size",
+                "size",
+                "font-style",
             ],
-            answer: "Event attributes"
+            answer: "font-size"
         },
-        {   num: 9,
+        {
+            num: 9,
             question: "Which attributes are neccessary for the &lt;img&gt; tag in HTML ?",
-            choices:[
-            "alt and data",
-            "data and source",
-            "src and alt",
-            "alt and source",
+            choices: [
+                "alt and data",
+                "data and source",
+                "src and alt",
+                "alt and source",
             ],
             answer: "src and alt"
         },
-        {   num: 10,
+        {
+            num: 10,
+            question: "How do you display hyperlinks without an underline ?",
+            choices: [
+                "a {underline:none;}",
+                "a {text-decoration:none;} ",
+                "a {decoration:no-underline;}",
+                "a {text-decoration:no-underline;}",
+            ],
+            answer: "a {text-decoration:none;} "
+        }
+    ],
+
+    [{
+            num: 1,
+            question: "The HTML &lt;canvas&gt; element is used to: ?",
+            choices: [
+                "draw graphics",
+                "manipulate data in MySQL",
+                "display database records",
+                "create draggable elements",
+            ],
+            answer: "draw graphics"
+        },
+        {
+            num: 2,
+            question: "In HTML, which attribute is used to specify that an input field must be filled out",
+            choices: [
+                "placeholder",
+                "validate",
+                "required",
+                "formvalidate",
+            ],
+            answer: "required"
+
+        },
+        {
+            num: 3,
+            question: "Which input type defines a slider control ?",
+            choices: [
+                "slider",
+                "controls",
+                "gauge",
+                "range",
+            ],
+            answer: "range"
+        },
+        {
+            num: 4,
+            question: "Which HTML attribute is used to define inline styles ?",
+            choices: [
+                "style",
+                "class",
+                "styles",
+                "font",
+            ],
+            answer: "style"
+        },
+        {
+            num: 5,
+            question: "Who is making the web standards ?",
+            choices: [
+                "Microsoft",
+                "Google",
+                "The World Wide Web Consortium",
+                "Mozilla",
+            ],
+            answer: "The World Wide Web Consortium"
+        },
+        {
+            num: 6,
+            question: "Which HTML attribute specifies an alternate text for an image, if the image cannot be displayed ?",
+            choices: [
+                "longdesc",
+                "alt",
+                "src",
+                "title",
+            ],
+            answer: "alt"
+        },
+        {
+            num: 7,
+            question: "Which property is used to change the background color in CSS ?",
+            choices: [
+                "background-color",
+                "backgroundColor",
+                "bgColor",
+                "color-background",
+            ],
+            answer: "background-color"
+        },
+        {
+            num: 8,
+            question: "In HTML, onblur and onfocus are: ?",
+            choices: [
+                "Style attributes",
+                "HTML elements",
+                "Can only be used in Javascript",
+                "Event attributes",
+            ],
+            answer: "Event attributes"
+        },
+        {
+            num: 9,
+            question: "Which attributes are neccessary for the &lt;img&gt; tag in HTML ?",
+            choices: [
+                "alt and data",
+                "data and source",
+                "src and alt",
+                "alt and source",
+            ],
+            answer: "src and alt"
+        },
+        {
+            num: 10,
             question: "Graphics defined by SVG is in which format ?",
-            choices:[
-            "HTML",
-            "XML",
-            "CSS",
-            "Javascript",
+            choices: [
+                "HTML",
+                "XML",
+                "CSS",
+                "Javascript",
             ],
             answer: "XML"
         }
--- assets/js/script.js
+++ assets/js/script.js
@@ -27,8 +27,8 @@ const nextlevel = document.getElementById('move-up');
 const playAgain = document.getElementById('start-again');
 const exBtn = document.getElementById('exit');
 
-let qLevelTime = [10,5];
-let level = [0,1];
+let qLevelTime = [10, 5];
+let level = [0, 1];
 let qlevel;
 let index = 0;
 let timer = qLevelTime[0];
@@ -43,7 +43,7 @@ qlevel = level[0];
 
 
 
-function quizLevel2(){
+function quizLevel2() {
     feedBack.classList.remove('reveal');
     quiz.classList.add('active');
     qlevel = level[1];
@@ -52,22 +52,22 @@ function quizLevel2(){
     index = 0;
     countTime(timer);
     loadQuestion(index);
-    questCounter(questC);  
+    questCounter(questC);
 }
 
-function userRegistration(){
+function userRegistration() {
     userReg.classList.add('show');
     rules.classList.add('inactive');
 }
 
 
-function validateName(){
+function validateName() {
     let regEx = /^[A-Za-z]+$/;
     if (username.value == '' || !(username.value.match(regEx))) {
         msg.classList.add('alert');
         username.focus();
         return false;
-        
+
     } else {
         return true;
     }
@@ -76,123 +76,123 @@ function validateName(){
 
 
 
-function goQuiz(){
+function goQuiz() {
     welcome.classList.remove('active');
     quiz.classList.add('active');
     countTime(timer);
     loadQuestion(index);
     questCounter(questC);
- }
+}
 
-function questCounter(questC){
+function questCounter(questC) {
     let qTag = `<span><p>${questC++}</p> <span><p>of </p></span><p>${dzQuiz[qlevel].length}</p><span><p>Questions</p></span>`;
     totalQ.innerHTML = qTag;
 }
 
-function countTime(timer){
+function countTime(timer) {
     counter = setInterval(timmer, 1000);
-    function timmer(){
+
+    function timmer() {
         timeSec.textContent = timer;
         timer--;
         if (timer < 0) {
             clearInterval(counter);
             timeSec.textContent = "00";
             for (let i = 0; i < options.length; i++) {
-                options[i].classList.add('disabled');  
+                options[i].classList.add('disabled');
             }
-        nextQ.style.display = 'block';
+            nextQ.style.display = 'block';
         }
     }
 }
 
-function loadQuestion(index){ 
+function loadQuestion(index) {
     questNum.innerHTML = `${dzQuiz[qlevel][index].num +'.'}`;
     questText.innerHTML = `${dzQuiz[qlevel][index].question}`;
     answer1.innerHTML = `${dzQuiz[qlevel][index].choices[0]}`;
     answer2.innerHTML = `${dzQuiz[qlevel][index].choices[1]}`;
     answer3.innerHTML = `${dzQuiz[qlevel][index].choices[2]}`;
     answer4.innerHTML = `${dzQuiz[qlevel][index].choices[3]}`;
-options.forEach(function(removeActive){
-    removeActive.classList.remove('correct');
-    removeActive.classList.remove('incorrect');
-    removeActive.classList.remove('disabled');
-    removeActive.classList.add('option');
-    removeActive.lastElementChild.innerHTML = '';
-});
+    options.forEach(function (removeActive) {
+        removeActive.classList.remove('correct');
+        removeActive.classList.remove('incorrect');
+        removeActive.classList.remove('disabled');
+        removeActive.classList.add('option');
+        removeActive.lastElementChild.innerHTML = '';
+    });
 
 
 }
 
-options.forEach(function(option){
-    option.addEventListener('click', function(){ 
-    clearInterval(counter);
-    let userAnswer = option.textContent;
-    let corTag = `<i class="fas fa-check"></i>`;
-    let incorTag = `<i class="fas fa-times"></i>`;
-    let correctAns = dzQuiz[qlevel][index].answer;
-   
-    if (userAnswer.trim() == correctAns.trim()) {
-        correct++;
-        option.lastElementChild.innerHTML = corTag;
-        option.classList.remove('option');
-        option.classList.add('correct');
-    } else {
-        option.lastElementChild.innerHTML = incorTag;
-        option.classList.add('incorrect');
-        for (let i = 0; i < options.length; i++) {
-            if (options[i].textContent.trim() == correctAns.trim()) {
-                options[i].setAttribute('class','option correct');
-                options[i].lastElementChild.innerHTML = corTag;
+options.forEach(function (option) {
+    option.addEventListener('click', function () {
+        clearInterval(counter);
+        let userAnswer = option.textContent;
+        let corTag = `<i class="fas fa-check"></i>`;
+        let incorTag = `<i class="fas fa-times"></i>`;
+        let correctAns = dzQuiz[qlevel][index].answer;
+
+        if (userAnswer.trim() == correctAns.trim()) {
+            correct++;
+            option.lastElementChild.innerHTML = corTag;
+            option.classList.remove('option');
+            option.classList.add('correct');
+        } else {
+            option.lastElementChild.innerHTML = incorTag;
+            option.classList.add('incorrect');
+            for (let i = 0; i < options.length; i++) {
+                if (options[i].textContent.trim() == correctAns.trim()) {
+                    options[i].setAttribute('class', 'option correct');
+                    options[i].lastElementChild.innerHTML = corTag;
+                }
+
             }
-            
         }
-    }
 
-     localStorage.setItem('correctAnswer', correct);
-    
-     clearInterval(interval);
+        localStorage.setItem('correctAnswer', correct);
 
-    for (let i = 0; i < options.length; i++) {
-        options[i].classList.add('disabled');  
-    }
-   
-    nextQ.style.display = 'block';
+        clearInterval(interval);
+
+        for (let i = 0; i < options.length; i++) {
+            options[i].classList.add('disabled');
+        }
+
+        nextQ.style.display = 'block';
 
 
     });
 });
 
-function nextQuestion(){
-    const  user = localStorage.getItem('username');
+function nextQuestion() {
+    const user = localStorage.getItem('username');
     const correctAnswer = localStorage.getItem('correctAnswer');
-    
+
     if (qlevel == 0 && correctAnswer >= 5) {
         face.innerHTML = `<i class="fas fa-trophy"></i>`;
         info.innerHTML = `Hi ${user}, congratulations you are good to go to the next level`;
         points.innerHTML = `You got ${correctAnswer} out of ${(qlevel+1)*10}`;
-        if (qlevel == 0 ) {
+        if (qlevel == 0) {
             nextlevel.style.display = 'block';
             exBtn.style.display = 'block';
             playAgain.style.display = 'none';
-        } 
-        
-    } else if (qlevel == 1 && correctAnswer >= 10){
+        }
+
+    } else if (qlevel == 1 && correctAnswer >= 10) {
         face.innerHTML = `<i class="fas fa-trophy"></i>`;
         info.innerHTML = `Hi ${user}, congratulations you are good.`;
         points.innerHTML = `You got ${correctAnswer} out of ${(qlevel+1)*10}`;
         exBtn.style.display = 'block';
         playAgain.style.display = 'none';
         nextlevel.style.display = 'none';
-    } 
-    else {
+    } else {
         face.innerHTML = `<i class="fas fa-meh-rolling-eyes"></i>`;
         info.innerHTML = `Hi ${user}, sorry you need to brush up your skills and try again.`;
         points.innerHTML = `You got ${correctAnswer} out of ${(qlevel+1)*10}`;
         exBtn.style.display = 'block';
         playAgain.style.display = 'block';
-        nextlevel.style.display = 'none';        
+        nextlevel.style.display = 'none';
     }
-    
+
     if (index < dzQuiz[qlevel].length - 1) {
         index++;
         questC++;
@@ -204,11 +204,11 @@ function nextQuestion(){
     } else {
         quiz.classList.remove('active');
         feedBack.classList.add('reveal');
-        
+
     }
 }
 
-function startAgain(){
+function startAgain() {
     feedBack.classList.remove('reveal');
     rules.classList.remove('inactive');
     correct = 0;
@@ -221,38 +221,38 @@ function startAgain(){
 
 continueBtn.addEventListener('click', userRegistration, false);
 
-rForm.addEventListener('submit', function(e){
+rForm.addEventListener('submit', function (e) {
     e.preventDefault();
     if (validateName()) {
         localStorage.setItem('username', username.value);
-        const  user = localStorage.getItem('username');
+        const user = localStorage.getItem('username');
         document.getElementById('user').textContent = user;
         welcome.classList.add('active');
         userReg.classList.remove('show');
     }
-    
-    
+
+
 });
 
-thankYou.addEventListener('click', function(e){
-    if(e == 'click'){
+thankYou.addEventListener('click', function (e) {
+    if (e == 'click') {
         goQuiz();
     }
 });
 
-nextQ.addEventListener('click', function(e){
+nextQ.addEventListener('click', function (e) {
     if (e == 'click') {
-        nextQuestion();        
+        nextQuestion();
     }
 });
 
-playAgain.addEventListener('click', function(e){
-    if (e  == 'click') {
+playAgain.addEventListener('click', function (e) {
+    if (e == 'click') {
         startAgain();
     }
 });
 
-nextlevel.addEventListener('click', function(e){
+nextlevel.addEventListener('click', function (e) {
     if (e == 'click') {
         quizLevel2();
     }
